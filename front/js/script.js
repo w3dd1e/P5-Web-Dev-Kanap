dc6486c7516393bc5b761df50f5268d53f2c5894
w3dd1e <kelly.weddle@gmail.com>
2023-11-08 22:37:16 -0600

    Fetch API product data in script.js

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+//GET product data from API URL
+
+const productAPI = "http://localhost:3000/api/products";
+
+fetch(productAPI).then((response) => {
+    if (!response.ok) {
+        throw new Error("Failed to retrieve product data.");
+    }
+    return response.json();
+});
