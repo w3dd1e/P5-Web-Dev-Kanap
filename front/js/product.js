f89c826070ab0ecc62a71a92b0ad53b6f46e7ae5
w3dd1e <kelly.weddle@gmail.com>
2023-11-18 12:56:23 -0600

    Update product page with fetched data from API

--- front/js/product.js
+++ front/js/product.js
@@ -10,3 +10,51 @@ const productColors = document.querySelector("#colors");
 const colorSelect = document.querySelector("#colors");
 const quantitySelect = document.querySelector("#quantity");
 const addToCart = document.querySelector("#addToCart");
+
+
+//Fetch Data from API then update page with returned data
+fetch(productAPI)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Failed to retrieve product data.");
+        }
+        return response.json();
+    })
+    .then((productData) => {
+        // Function to insert product data into page by each element selector
+        function setElement(selector, attribute, value) {
+            const element = document.querySelector(selector);
+            if (attribute === "textContent") {
+                element.textContent = value;
+            } else {
+                element.setAttribute(attribute, value);
+            }
+        }
+        // Update page title
+        document.title = productData.name;
+
+        // Insert product name, price, and description
+        setElement("#title", "textContent", productData.name);
+        setElement("#price", "textContent", productData.price);
+        setElement("#description", "textContent", productData.description);
+
+        // Insert product image into DOM
+        let productImage = document.createElement("img");
+        imageContainer.append(productImage);
+        setElement(".item__img img", "src", productData.imageUrl);
+        setElement(".item__img img", "alt", productData.altTxt);
+
+        // Insert product color options
+        for (let colorOption of productData.colors) {
+            let newOption = document.createElement("option");
+            newOption.value = colorOption;
+            newOption.textContent = colorOption;
+            productColors.append(newOption);
+        }
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+
+
+ 
