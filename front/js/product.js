414462b88c066c4c1271f252ef0665f4e07e8c67
w3dd1e <kelly.weddle@gmail.com>
2023-11-18 12:54:42 -0600

    Create product.js and identify DOM variables

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,12 @@
-// Get URL from current page
+//Exisiting DOM elements
 const url = new URL(window.location.href);
-//Get product ID from URL
 const urlParams = new URLSearchParams(url.search);
 const productID = urlParams.get("id");
-
-//GET product data from API URL
 const productAPI = `http://localhost:3000/api/products/${productID}`;
 
-fetch(productAPI)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Failed to retrieve product data.");
-        }
-        return response.json();
-    })
-    .then((productData) => {
-        // Function to insert product data into page by each element selector
-        function setElement(selector, attribute, value) {
-            const element = document.querySelector(selector);
-            if (attribute === "textContent") {
-                element.textContent = value;
-            } else {
-                element.setAttribute(attribute, value);
-            }
-        }
-        // Update page title
-        document.title = productData.name;
+const imageContainer = document.querySelector(".item__img");
+const productColors = document.querySelector("#colors");
 
-        // Insert product name, price, and description
-        setElement("#title", "textContent", productData.name);
-        setElement("#price", "textContent", productData.price);
-        setElement("#description", "textContent", productData.description);
-
-        // Insert product image into DOM
-        const imageContainer = document.querySelector(".item__img");
-        let productImage = document.createElement("img");
-        imageContainer.append(productImage);
-        setElement(".item__img img", "src", productData.imageUrl);
-        setElement(".item__img img", "alt", productData.altTxt);
-
-        // Insert product color options
-        const productColors = document.querySelector("#colors");
-        for (let color of productData.colors) {
-            let newOption = document.createElement("option");
-            newOption.value = color;
-            newOption.textContent = color;
-            productColors.append(newOption);
-        }
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+const colorSelect = document.querySelector("#colors");
+const quantitySelect = document.querySelector("#quantity");
+const addToCart = document.querySelector("#addToCart");
