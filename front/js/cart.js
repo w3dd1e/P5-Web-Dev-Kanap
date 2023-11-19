aa75311f00d77c3b9b1826a4ea72f1df9db3cba7
w3dd1e <kelly.weddle@gmail.com>
2023-11-19 10:22:53 -0600

    Add  items to cart list from session storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,20 @@
 //DOM Variables
 const cartList = document.querySelector("#cart__items");
 
-
 //Use localStorage as cart variable for easier readability
-let cartStoredProduct = Object.keys(sessionStorage);
-let cartStoredQtys = Object.values(sessionStorage);
+let cart = sessionStorage
+let cartProducts = Object.keys(cart);
+let cartQtys = Object.values(cart);
+
+//Product Variables
 let product ={};
 let productID = "";
+let productData = {};
 
-for(let item in cartStoredProduct){
-  product = JSON.parse(cartStoredProduct[item]);
-  productID = product.id;}
+for(let item in cartProducts){
+  product = JSON.parse(cartProducts[item]);
+  quantity = cartQtys[item];
+  productID = product.id;
 
   //API Data Variables
 const productAPI = `http://localhost:3000/api/products/${productID}`;
@@ -22,30 +26,25 @@ fetch(productAPI)
         }
         return response.json();
     })
+    
     .then((data) => { 
-     window['productData'+`${productID}`] = data;
-     })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
-
-
-
+    productData = data;
+   
     let newProductCard = `
     <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
     <div class="cart__item__img">
-      <img src="${productData[product.id].imageUrl}" alt="${productData[product.id].altTxt}">
+      <img src="${productData.imageUrl}" alt="${productData.altTxt}">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__description">
-        <h2>Name of the product</h2>
-        <p>Green</p>
-        <p>€42.00</p>
+        <h2>${productData.name}</h2>
+        <p>${product.color}</p>
+        <p>€${productData.price}</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Quantity : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
           <p class="deleteItem">Delete</p>
@@ -56,10 +55,10 @@ fetch(productAPI)
     `;
 
     cartList.insertAdjacentHTML("beforeend", newProductCard);
+
+  })
+  .catch((error) => {
+      console.error("Error:", error);
+  });
+
 };
-
-
-
-
-
-
