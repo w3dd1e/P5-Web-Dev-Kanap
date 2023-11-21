c5a6c56d7688fa0bd825d8c411fb478b4740ef0f
w3dd1e <kelly.weddle@gmail.com>
2023-11-20 22:30:01 -0600

    Fix duplicate code after braches merged

--- front/js/cart.js
+++ front/js/cart.js
@@ -35,13 +35,9 @@ fetch(productAPI)
     <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
     <div class="cart__item__img">
       <img src="${productData.imageUrl}" alt="${productData.altTxt}">
-      <img src="${productData.imageUrl}" alt="${productData.altTxt}">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__description">
-        <h2>${productData.name}</h2>
-        <p>${product.color}</p>
-        <p>€${productData.price}</p>
         <h2>${productData.name}</h2>
         <p>${product.color}</p>
         <p>€${productData.price}</p>
@@ -49,7 +45,6 @@ fetch(productAPI)
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Quantity : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
           <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
