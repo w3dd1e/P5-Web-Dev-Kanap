eb025c65bb9f9f04147198e9b0ccc76232f0b6ea
w3dd1e <kelly.weddle@gmail.com>
2023-11-21 00:50:30 -0600

    Calculate total items and insert in DOM

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,35 +5,37 @@ const cartTotalPrice = document.querySelector("#totalPrice");
 
 //Use localStorage as cart variable for easier readability
 const cartStoredProduct = Object.keys(sessionStorage);
-let product ={};
-let productID = "";
 const cartStoredQtys = Object.values(sessionStorage);
+
+let product = {};
+let productID = "";
 let quantity = "";
+let priceArray = [];
 
 //Loop through cart list to display each item on page
-for(let item in cartStoredProduct){
-  product = JSON.parse(cartStoredProduct[item]);
-  productID = product.id;
+for (let item in cartStoredProduct) {
+    product = JSON.parse(cartStoredProduct[item]);
+    productID = product.id;
 
-  //API Data Variables
-const productAPI = `http://localhost:3000/api/products/${productID}`;
+    //API Data Variables
+    const productAPI = `http://localhost:3000/api/products/${productID}`;
 
-fetch(productAPI)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Failed to retrieve product data.");
-        }
-        return response.json();
-    })
-    
-    .then((data) => { 
-      productData = data;
+    fetch(productAPI)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to retrieve product data.");
+            }
+            return response.json();
+        })
 
-      //This variable must be repeated in this callback to prevent varible from changing on loop before being applied to DOM due to async nature of fetch
-      product = JSON.parse(cartStoredProduct[item]);
-      quantity = cartStoredQtys[item];
-     
-    let newProductCard = `
+        .then((data) => {
+            productData = data;
+
+            //This variable must be repeated in this callback to prevent varible from changing on loop before being applied to DOM due to async nature of fetch
+            product = JSON.parse(cartStoredProduct[item]);
+            quantity = cartStoredQtys[item];
+
+            let newProductCard = `
     <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
       <div class="cart__item__img">
         <img src="${productData.imageUrl}" alt="${productData.altTxt}">
@@ -57,14 +59,15 @@ fetch(productAPI)
     </article>
     `;
 
-    cartList.insertAdjacentHTML("beforeend", newProductCard);
+            cartList.insertAdjacentHTML("beforeend", newProductCard);
+            priceArray.push(productData.price);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
 
-  })
-.catch((error) => {
-  console.error("Error:", error);
-});
-};
-
-
-
-
+//Calculate total items and insert into DOM
+cartQtys = cartStoredQtys.map((item) => Number(item));
+let totalItems = cartQtys.reduce((sum, current) => sum + current, 0);
+cartTotalItems.textContent = totalItems;
