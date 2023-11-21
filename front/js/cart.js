f11a90223bcbda284c8a8a6e07c4c3ff19ee2b8f
w3dd1e <kelly.weddle@gmail.com>
2023-11-20 22:23:29 -0600

    Refactor cart display logic to show qty and color

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,17 @@
 //DOM Variables
 const cartList = document.querySelector("#cart__items");
 
-
 //Use localStorage as cart variable for easier readability
-let cartStoredProduct = Object.keys(sessionStorage);
-let cartStoredQtys = Object.values(sessionStorage);
+const cartProduct = Object.keys(sessionStorage);
 let product ={};
 let productID = "";
+const cartQtys = Object.values(sessionStorage);
+let quantity = "";
 
-for(let item in cartStoredProduct){
-  product = JSON.parse(cartStoredProduct[item]);
-  productID = product.id;}
+//Loop through cart list to display each item on page
+for(let item in cartProduct){
+  product = JSON.parse(cartProduct[item]);
+  productID = product.id;
 
   //API Data Variables
 const productAPI = `http://localhost:3000/api/products/${productID}`;
@@ -23,29 +24,27 @@ fetch(productAPI)
         return response.json();
     })
     .then((data) => { 
-     window['productData'+`${productID}`] = data;
-     })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+      productData = data;
 
-
-
+      //This variable must be repeated in this callback to prevent varible from changing on loop before being applied to DOM due to async nature of fetch
+      product = JSON.parse(cartProduct[item]);
+      quantity = cartQtys[item];
+     
     let newProductCard = `
     <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
     <div class="cart__item__img">
-      <img src="${productData[product.id].imageUrl}" alt="${productData[product.id].altTxt}">
+      <img src="${productData.imageUrl}" alt="${productData.altTxt}">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__description">
-        <h2>Name of the product</h2>
-        <p>Green</p>
-        <p>€42.00</p>
+        <h2>${productData.name}</h2>
+        <p>${product.color}</p>
+        <p>€${productData.price}</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Quantity : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
           <p class="deleteItem">Delete</p>
@@ -56,8 +55,12 @@ fetch(productAPI)
     `;
 
     cartList.insertAdjacentHTML("beforeend", newProductCard);
-};
 
+  })
+.catch((error) => {
+  console.error("Error:", error);
+});
+};
 
 
 
