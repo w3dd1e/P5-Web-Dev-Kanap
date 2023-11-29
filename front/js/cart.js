0886448d2e0354c18a05473d2a2364170faa5c91
w3dd1e <kelly.weddle@gmail.com>
2023-11-29 01:40:09 -0600

    Refactor cart  and update functionality -Rewrite code for better readability -Add function to calculate totals on input changes

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,80 +1,114 @@
 //DOM Variables
 const cartList = document.querySelector("#cart__items");
-const cartTotalItems = document.querySelector("#totalQuantity");
-const cartTotalPrice = document.querySelector("#totalPrice");
 
-//Use localStorage as cart variable for easier readability
-const cartStoredProduct = Object.keys(sessionStorage);
-const cartStoredQtys = Object.values(sessionStorage);
+//Storage Variables
+const cart = sessionStorage;
+const savedProducts = Object.keys(sessionStorage);
+const savedQtys = Object.values(sessionStorage);
 
-let product = {};
-let productID = "";
-let quantity = "";
-let priceArray = [];
+//Product Variables
+let productPrices = {};
+let totalItems = 0;
+let totalPrice = 0;
 
-//Loop through cart list to display each item on page
-for (let item in cartStoredProduct) {
-    product = JSON.parse(cartStoredProduct[item]);
-    productID = product.id;
-
-    //API Data Variables
-    const productAPI = `http://localhost:3000/api/products/${productID}`;
-
-    fetch(productAPI)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Failed to retrieve product data.");
-            }
-            return response.json();
-        })
-
-        .then((data) => {
-            productData = data;
-
-            //This variable must be repeated in this callback to prevent varible from changing on loop before being applied to DOM due to async nature of fetch
-            product = JSON.parse(cartStoredProduct[item]);
-            quantity = cartStoredQtys[item];
-
-            let newProductCard = `
-    <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
-      <div class="cart__item__img">
-        <img src="${productData.imageUrl}" alt="${productData.altTxt}">
-      </div>
-      <div class="cart__item__content">
-        <div class="cart__item__content__description">
-          <h2>${productData.name}</h2>
-          <p>${product.color}</p>
-          <p>€${productData.price}</p>
-        </div>
-        <div class="cart__item__content__settings">
-          <div class="cart__item__content__settings__quantity">
-            <p>Quantity : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
-          </div>
-          <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Delete</p>
-          </div>
-        </div>
-      </div>
-    </article>
-    `;
-
-            cartList.insertAdjacentHTML("beforeend", newProductCard);
-            priceArray.push(productData.price);
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
+//Function to set element values
+function setElement(selector, value) {
+    const element = document.querySelector(selector);
+    element.textContent = value;
 }
 
-//Calculate total items and insert into DOM
-cartQtys = cartStoredQtys.map((item) => Number(item));
-let totalItems = cartQtys.reduce((sum, current) => sum + current, 0);
-cartTotalItems.textContent = totalItems;
+//Fetch product data from API
+async function fetchProducts(productID) {
+    const response = await fetch(
+        `http://localhost:3000/api/products/${productID}`
+    );
+    const data = await response.json();
+    return data;
+}
 
-//Calculate total price and insert into DOM
-let totalPrice = priceArray.map((item, id) => item * cartQtys[id]);
-cartTotalPrice.textContent = totalPrice.reduce(
-    (sum, current) => sum + current,
-    0
-);
+//Loop through cart list to display each item on page
+for (let item in savedProducts) {
+    const productID = JSON.parse(savedProducts[item]);
+    fetchProducts(productID.id).then((data) => {
+        // Calculate quantities and total price per product
+        productPrices[data._id] = data.price;
+        let quantity = savedQtys[item];
+        const itemPrice = data.price * quantity;
+
+        // Create and insert HTML for each item in cart
+        const cartItem = document.createElement("div");
+        cartItem.classList.add("cart__item");
+        cartItem.innerHTML = `<article class="cart__item" data-id="${data._id}" data-color="${productID.color}">
+          <div class="cart__item__img">
+            <img src="${data.imageUrl}" alt="${data.altTxt}">
+          </div>
+          <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${data.name}</h2>
+              <p>${productID.color}</p>
+              <p>€${data.price}</p>
+            </div>
+            <div class="cart__item__content__settings">
+              <div class="cart__item__content__settings__quantity">
+                <p>Quantity : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
+              </div>
+              <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Delete</p>
+              </div>
+            </div>
+          </div>
+        </article>`;
+        cartList.appendChild(cartItem);
+
+        // Update total items and total price
+        totalItems += Number(quantity);
+        totalPrice += itemPrice;
+        setElement("#totalQuantity", totalItems);
+        setElement("#totalPrice", `${totalPrice.toFixed(2)}`);
+    });
+}
+
+//Update cart on input changes
+cartList.addEventListener("change", (event) => {
+    if (event.target.classList.contains("itemQuantity")) {
+        alert("Quantity changed");
+
+        //Product Variables from DOM
+        let currentQtys = document.querySelectorAll(".itemQuantity");
+        let product = event.target.closest("article");
+        let price = product.querySelector(
+            ".cart__item__content__description p:nth-child(3)"
+        ).textContent;
+        let quantity = event.target.value;
+        let colorOption = product.dataset.color;
+        let productID = product.dataset.id;
+
+        //Update stored cart quantities
+        let item = { id: productID, color: colorOption };
+        let cartItem = JSON.stringify(item);
+        cart[cartItem] = quantity;
+
+        //Get current quantity values from DOM
+        let values = Object.values(currentQtys);
+
+        //Calculate new totals and insert in DOM
+        totalItems = 0;
+        totalPrice = 0;
+        for (let input in values) {
+            let quantity = values[input].value;
+            totalItems += Number(quantity);
+            setElement("#totalQuantity", totalItems);
+
+            let priceElement = values[input]
+                .closest("article")
+                .querySelector(
+                    ".cart__item__content__description p:nth-child(3)"
+                );
+            let price = priceElement.textContent;
+            let itemPrice = Number(price.replace("€", "")) * Number(quantity);
+            totalPrice += itemPrice;
+            setElement("#totalPrice", `${totalPrice.toFixed(2)}`);
+        }
+    }
+});
