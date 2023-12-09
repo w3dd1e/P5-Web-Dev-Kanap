d9d33a84da446dcf0cbbf648b6f7b7dc2c6fcb87
w3dd1e <kelly.weddle@gmail.com>
2023-12-09 15:01:16 -0600

    Update comments on confirmation page

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,7 @@
+//Get Order ID from URL
 const url = new URL(window.location.href);
 const urlParams = new URLSearchParams(url.search);
 const orderID = urlParams.get("orderId");
 
+//Display Order ID on page
 document.getElementById("orderId").textContent = `${orderID}`;
